e68503d1c0e22498f0373eb36bd2fe3552f9bdcb
bharat.kumar@dotsquares.com-at-264857186093 <bharat.kumar@dotsquares.com>
2025-05-26 10:21:32 +0530

    handle login admin redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useEffect } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { getAccessToken } from "./store/auth/AuthExtraReducers.jsx";
@@ -6,7 +6,6 @@ import { getAccessToken } from "./store/auth/AuthExtraReducers.jsx";
 import "../public/assets/css/app.min.css";
 import "../public/assets/css/icons.min.css";
 import "../public/assets/css/vendor.min.css";
-
 import "../public/assets/js/vendor.js";
 
 import PageLoader from "./components/PageLoader/index.jsx";
@@ -14,7 +13,6 @@ import "./index.scss";
 import LoginUser from "./components/unauthorized/LoginUser.jsx";
 import SignUpUser from "./components/unauthorized/SignupUser.jsx";
 import FatperchantageCalculator from "./components/pages/FatperchantageCalculator.jsx";
-import Tools from "./components/pages/Tools.jsx";
 
 const UserRoutes = lazy(() => import("./components/Routes/UserRoutes.jsx"));
 const AdminRoutes = lazy(() => import("./components/Routes/AdminRoutes.jsx"));
@@ -28,6 +26,7 @@ const ProtectedRoute = ({ condition, redirectTo = "/LoginUser", children }) => {
 
 const App = () => {
   const dispatch = useDispatch();
+  const [isAdminLocal, setIsAdminLocal] = useState(false)
   const { isCheckingToken, isLoggedIn, isAdmin } = useSelector(
     (state) => state.auth
   );
@@ -43,9 +42,9 @@ const App = () => {
     return (
       <Suspense fallback={<PageLoader />}>
         <Routes>
-          <Route path="/admin" element={<LoginUser />} />
+          <Route path="/admin" element={<LoginUser setIsAdminLocal={setIsAdminLocal} />} />
           <Route path="/forgot-password" element={<ForgotPasswordForm />} />
-          <Route path="LoginUser" element={<LoginUser />} />
+          <Route path="LoginUser" element={<LoginUser setIsAdminLocal={setIsAdminLocal} />} />
           <Route path="SignUpUser" element={<SignUpUser />} />
           <Route path="/*" element={<UserRoutes />} />
           <Route path="*" element={<Navigate replace to="/*" />} />
@@ -64,7 +63,7 @@ const App = () => {
         <Route
           path="/admin/*"
           element={
-            <ProtectedRoute condition={isAdmin} redirectTo="/">
+            <ProtectedRoute condition={isAdminLocal||isAdmin} redirectTo="/">
               <AdminRoutes />
             </ProtectedRoute>
           }
